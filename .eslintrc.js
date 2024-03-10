c20cdd94a1ebeff811f360bb64d28a2359341017
Colin Maher <colinmaher94@gmail.com>
2024-03-10 13:52:56 +0100

    add game icons

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime'],
-  rules: {
-    'react/display-name': 'off',
-    'react/no-unknown-property': 'off'
-  }
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime']
 }
